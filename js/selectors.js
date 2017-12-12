fcac0dec66ffb91734bd66798ec3c9154a73566a
Rani <rani76@gmail.com>
2017-12-12 10:46:19 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,15 @@
-// declare your functions here...
+paragraphSelector() {
+  $('p')
+}
+lastImageSelector() {
+  $('img:last')
+}
+ninjaBabySelector() {
+  $("img[id='baby-ninja']")
+}
+divSelector() {
+  $('div class=pics')
+}
+firstListItem() {
+$("ul[id='pic-list'] ul:first-child")
+}
